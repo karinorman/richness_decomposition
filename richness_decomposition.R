e68a9356a9056ba32b52a46d629d2c6445792fa3
Kari Norman <kari@normanfamily.org>
2017-03-03 15:04:24 -0800

    Create species by site matrix using spread() function.

--- richness_decomposition.R
+++ richness_decomposition.R
@@ -1,27 +1,9 @@
 library(tidyverse)
-bbs <- read_csv("bbs_abundances_by_site.csv", col_types = "iddii")
+bbs <- read_csv("bbs_abundances_by_site.csv")
 
-species_by_site <- as.data.frame(matrix(0, ncol = length(unique(bbs$species)), nrow = length(unique(bbs$site))))
-names(species_by_site) <- as.integer(unique(bbs$species))
-
-#Species by site matrix
-site_ID <- unique(bbs$site)
-for (i in 1:length(site_ID)) {
-  site_data <- subset(bbs, site == site_ID[i])
-  for (j in 1:length(site_data)){
-    species <- toString(site_data$species[j])
-    species_by_site[i,species] <- 1
-  }
-}
-
-# bbs %>%
-#   for (i in 1:3){
-#     filter(bbs$site == site_ID[i]) %>%
-#       for (j in 1:length(site)){
-#         species <- toString(site[j])
-#         species_by_site$species[i] <- 1
-#       }
-#   }
+bbs %>%
+  select(site, species, count) %>%
+  spread(key = species, value = count) -> species_by_site
 
 #Species cooccurence matrix using sorensen-based dissimilarity coefficient
 prac <- matrix(rbinom(24, 1, .5), ncol = 4)
@@ -49,5 +31,5 @@ for (i in 1:ncol(pracdf)){
   }
 }
 
-sorenson <- dissimilarity/((2*a_matrix)+dissimilarity))
+sorenson <- dissimilarity/((2*a_matrix)+dissimilarity)
 
