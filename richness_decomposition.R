d460a854fa7762c0db59dfa9d20a40d294123910
Kari Norman <kari@normanfamily.org>
2017-03-13 11:40:22 -0700

    Get martirx of sorenson dissimilarity by function.

--- richness_decomposition.R
+++ richness_decomposition.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(ecosim)
+
 bbs <- read_csv("bbs_abundances_by_site.csv")
 
 bbs %>%
@@ -10,26 +12,37 @@ prac <- matrix(rbinom(24, 1, .5), ncol = 4)
 pracdf <- as.data.frame(prac)
 names(pracdf) <- c('a','b','c','d')
 
-a_matrix <- matrix(nrow = ncol(pracdf), ncol = ncol(pracdf))
-bc_matrix <- matrix(nrow = ncol(pracdf), ncol = ncol(pracdf))
 
-for (i in 1:ncol(pracdf)){
-  df <- subset(pracdf, pracdf[,i] == 1)
-  a = colSums(df[,i]==df)
-  a_matrix[i,] <- a
-  b = dim(df)[1]-a
-  bc_matrix[i,] <- b
-}
-
-dissimilarity <- matrix(nrow = ncol(pracdf), ncol = ncol(pracdf))
-
-for (i in 1:ncol(pracdf)){
-  for(j in 1:ncol(pracdf)){
-    b_c <- bc_matrix[i,j] + bc_matrix[j,i]
-    dissimilarity[i,j] <- b_c
-    dissimilarity[j,i] <- b_c
+get_sorenson_matrix <- function(cooccurrence_df) {
+  a_matrix <-
+    matrix(nrow = ncol(cooccurrence_df),
+           ncol = ncol(cooccurrence_df))
+  bc_matrix <-
+    matrix(nrow = ncol(cooccurrence_df),
+           ncol = ncol(cooccurrence_df))
+  
+  for (i in 1:ncol(cooccurrence_df)) {
+    df <- subset(cooccurrence_df, cooccurrence_df[, i] == 1)
+    a = colSums(df[, i] == df)
+    a_matrix[i, ] <- a
+    b = dim(df)[1] - a
+    bc_matrix[i, ] <- b
   }
+  
+  dissimilarity <-
+    matrix(nrow = ncol(cooccurrence_df),
+           ncol = ncol(cooccurrence_df))
+  
+  for (i in 1:ncol(cooccurrence_df)) {
+    for (j in 1:ncol(cooccurrence_df)) {
+      b_c <- bc_matrix[i, j] + bc_matrix[j, i]
+      dissimilarity[i, j] <- b_c
+      dissimilarity[j, i] <- b_c
+    }
+  }
+  
+  sorenson <- dissimilarity / ((2 * a_matrix) + dissimilarity)
+  return(sorenson)
 }
 
-sorenson <- dissimilarity/((2*a_matrix)+dissimilarity)
-
+prac_sor <- get_sorenson_matrix(pracdf)
